5f48cffe3d8705ec78111ea1bebaf09786cbfcf7
brucepei <brucepei@hotmail.com>
2016-03-29 20:33:48 +0800

    add unit test for Host model

--- tbd/tests.py
+++ tbd/tests.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.utils import timezone
-from tbd.models import Project, Build, TestData
+from tbd.models import Project, Build, Host#, TestData
 from tbd.views import home_page, project_page, build_page
 from datetime import datetime
 from .forms import AddProjectForm, AddBuildForm
@@ -239,13 +239,13 @@ class TBDModelTest(TestCase):
         self.assertEqual(second_prj.owner, prj2.owner)
         
     def test_saving_and_retrieve_build(self):
-        prj1 = Project(name="unit_project1", owner="test1")
+        prj1 = Project(name="unit_project1", owner="tester1")
         prj1.save()
-        prj2 = Project(name="unit_project2", owner="test1")
+        prj2 = Project(name="unit_project2", owner="tester1")
         prj2.save()
-        build1 = Build(version="unit_project1", project=prj1)
+        build1 = Build(version="unit_build1", project=prj1)
         build1.save()
-        build2 = Build(version="unit_project2", project=prj2)
+        build2 = Build(version="unit_build2", project=prj2)
         build2.save()
         
         saved_items = Build.objects.all()
@@ -258,18 +258,38 @@ class TBDModelTest(TestCase):
         self.assertEqual(second_build.version, build2.version)
         self.assertEqual(second_build.project.name, build2.project.name)
         
-    def test_saving_and_retrieve_testdata(self):
-        prj1 = Project(name="unit_project1", owner="test1")
+    def test_saving_and_retrieve_host(self):
+        host1 = Host(name="hostname1", ip="1.1.1.1")
+        host1.save()
+        host2 = Host(name="hostname2", ip="1.1.1.2")
+        host2.save()
+        
+        saved_items = Host.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_host = saved_items[0]
+        second_host = saved_items[1]
+        self.assertEqual(first_host.name, host1.name)
+        self.assertEqual(first_host.ip, host1.ip)
+        self.assertEqual(second_host.name, host2.name)
+        self.assertEqual(second_host.ip, host2.ip)
+        
+    def atest_saving_and_retrieve_testdata(self):
+        prj1 = Project(name="unit_project1", owner="tester1")
         prj1.save()
-        prj2 = Project(name="unit_project2", owner="test1")
+        prj2 = Project(name="unit_project2", owner="tester1")
         prj2.save()
         build1 = Build(version="unit_project1", project=prj1)
         build1.save()
         build2 = Build(version="unit_project2", project=prj2)
         build2.save()
-        td1 = TestData(testcase="test_case1", project=prj1, build=build1)
+        host1 = Host(name="hostname1", ip="1.1.1.1")
+        host1.save()
+        host2 = Host(name="hostname2", ip="1.1.1.2")
+        host2.save()
+        td1 = TestData(testcase="test_case1", host=host1, build=build1)
         td1.save()
-        td2 = TestData(testcase="test_case2", project=prj2, build=build2)
+        td2 = TestData(testcase="test_case2", host=host2, build=build2)
         td2.save()
         
         saved_items = TestData.objects.all()
@@ -278,8 +298,11 @@ class TBDModelTest(TestCase):
         first_td = saved_items[0]
         second_td = saved_items[1]
         self.assertEqual(first_td.testcase, td1.testcase)
-        self.assertEqual(first_td.project.name, td1.project.name)
+        self.assertEqual(first_td.build.project.name, td1.build.project.name)
         self.assertEqual(first_td.build.version, td1.build.version)
+        self.assertEqual(first_td.host.name, td1.host.name)
+        
         self.assertEqual(second_td.testcase, td2.testcase)
-        self.assertEqual(second_td.project.name, td2.project.name)
+        self.assertEqual(second_td.build.project.name, td2.build.project.name)
         self.assertEqual(second_td.build.version, td2.build.version)
+        self.assertEqual(second_td.host.name, td2.host.name)
