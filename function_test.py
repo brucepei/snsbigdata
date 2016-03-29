d1719b099c2a8e23a7d0838efd99efa0101480d5
brucepei <brucepei@hotmail.com>
2016-03-29 19:43:30 +0800

    supply func test for Build page

--- function_test.py
+++ function_test.py
@@ -12,18 +12,18 @@ class TBDTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
-    def atest_homepage_title(self):
+    def test_homepage_title(self):
         self.browser.get(self.live_server_url)
         self.assertIn('SnS Big Data', self.browser.title)
     
-    def atest_homepage_links(self):
+    def test_homepage_links(self):
         self.browser.get(self.live_server_url)
         links = self.browser.find_elements_by_tag_name('a')
         links_text = [link.text for link in links]
         self.assertIn('Home', links_text)
         self.assertIn('Project', links_text)
     
-    def atest_static_bootstap(self):
+    def test_static_bootstap(self):
         self.browser.get(self.live_server_url + '/static/css/bootstrap.min.css')
         self.assertNotIn('Page not found', self.browser.title)
         self.browser.get(self.live_server_url + '/static/js/jquery-1.11.3.min.js')
@@ -31,7 +31,7 @@ class TBDTest(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url + '/static/js/bootstrap.min.js')
         self.assertNotIn('Page not found', self.browser.title)
     
-    def atest_project_form(self):
+    def test_project_form(self):
         self.browser.get(self.live_server_url + '/project')
         target_project = self.browser.find_element_by_id('id_project_name')
         target_project.send_keys("test_func")
@@ -55,7 +55,7 @@ class TBDTest(StaticLiveServerTestCase):
         self.assertEqual(len(rows), 2, "should not duplicated with the same prj name")
         self.assertIn('test_func tester', rows[1].text)
 
-        self.browser.find_element_by_css_selector("#id_project_table tr td a").click()
+        self.browser.find_element_by_css_selector("#id_project_table tr td a span.glyphicon-remove").click()
         project_table = self.browser.find_element_by_id('id_project_table')
         rows = project_table.find_elements_by_tag_name('tr')
         self.assertEqual(len(rows), 1)
@@ -71,8 +71,8 @@ class TBDTest(StaticLiveServerTestCase):
         
         self.browser.get(self.live_server_url + '/build?project_name={}'.format('test_func'))
         
-        project_table = self.browser.find_element_by_id('id_build_table')
-        rows = project_table.find_elements_by_tag_name('tr')
+        build_table = self.browser.find_element_by_id('id_build_table')
+        rows = build_table.find_elements_by_tag_name('tr')
         self.assertEqual(len(rows), 1)
         self.assertIn('ID Version ShortName UseServer Create Operation', rows[0].text)
         
@@ -88,8 +88,8 @@ class TBDTest(StaticLiveServerTestCase):
         target_project.send_keys("\\\\l\\v1")
         target_project.send_keys(Keys.ENTER)
         self.browser.refresh()
-        project_table = self.browser.find_element_by_id('id_build_table')
-        rows = project_table.find_elements_by_tag_name('tr')
+        build_table = self.browser.find_element_by_id('id_build_table')
+        rows = build_table.find_elements_by_tag_name('tr')
         self.assertEqual(len(rows), 5)
         self.assertIn('version001 v1 False', rows[1].text)
         
@@ -106,13 +106,13 @@ class TBDTest(StaticLiveServerTestCase):
         target_project.send_keys(Keys.ENTER)
         self.browser.refresh()
         
-        project_table = self.browser.find_element_by_id('id_build_table')
-        rows = project_table.find_elements_by_tag_name('tr')
+        build_table = self.browser.find_element_by_id('id_build_table')
+        rows = build_table.find_elements_by_tag_name('tr')
         self.assertEqual(len(rows), 5, "should not duplicated with the same prj and version")
         self.assertIn('version001 v1 False', rows[1].text)
 
 
-        # self.browser.find_element_by_css_selector("#id_project_table tr td a").click()
-        # project_table = self.browser.find_element_by_id('id_project_table')
-        # rows = project_table.find_elements_by_tag_name('tr')
-        # self.assertEqual(len(rows), 1)
+        self.browser.find_element_by_css_selector("#id_build_table tr td a span.glyphicon-remove").click()
+        build_table = self.browser.find_element_by_id('id_build_table')
+        rows = build_table.find_elements_by_tag_name('tr')
+        self.assertEqual(len(rows), 1)
