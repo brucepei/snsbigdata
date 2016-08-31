e30d3d5d583968408591242d4040b728a892be93
brucepei <brucepei@hotmail.com>
2016-08-31 11:44:16 +0800

    using csrf token from page but not cookie

--- tbd/static/tbd/js/user_js.js
+++ tbd/static/tbd/js/user_js.js
@@ -10,6 +10,15 @@
         }
         return attributes;
     };
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                //alert($("#csrfmiddlewaretoken").val());
+                xhr.setRequestHeader("X-CSRFToken", $("#csrfmiddlewaretoken").val());
+            }
+        }
+    });
 })(jQuery);
 
 var get_cookie = function (name) {  
@@ -85,9 +94,6 @@ var ajax_post_data = function(url, data, success_code) {
             data: data,
             timeout: 2000,
             dataType: "json",
-            beforeSend: function(xhr, settings){  
-                xhr.setRequestHeader("X-CSRFToken", get_cookie('csrftoken'));  
-            },
             success: function (json) {
                 if ('code' in json && 'result' in json) {
                     if (json.code) {
@@ -182,165 +188,6 @@ $("select[name='testcase']").change( function() {
     }
 });
 
-$("select[name='td_project_name']").change( function() {
-    var $td_build_version_sel = $("select[name='td_build_version']");
-    var get_build_url = '/ajax/get_builds'
-    var prj_name = $(this).find("option:selected").attr('data-name');
-    cur_url = window.location.href;
-    if(cur_url.indexOf('/testdata')) {
-        var sort_by = $("select[name='td_sort_by']").val();
-        project_url = prj_name ? '?project_name=' + prj_name + "&sort_by=" + sort_by : "?sort_by=" + sort_by;
-        window.location.href = '/testdata' + project_url;
-    }
-});
-
-$("select[name='td_build_version']").change( function() {
-    var prj_name = $("select[name='td_project_name']").find("option:selected").attr('data-name');
-    var build_version = $(this).find("option:selected").attr('data-version');
-    cur_url = window.location.href;
-    if(cur_url.indexOf('/testdata')) {
-        if (build_version) {
-            var sort_by = $("select[name='td_sort_by']").val();
-            window.location.href = '/testdata?project_name=' + prj_name + "&version=" + build_version + "&sort_by=" + sort_by;
-        }
-    }
-});
-
-$("select[name='td_sort_by']").change( function() {
-    var prj_name = $("select[name='td_project_name']").find("option:selected").attr('data-name');
-    var build_version = $("select[name='td_build_version']").find("option:selected").attr('data-version');
-    var sort_by = $(this).val();
-    cur_url = window.location.href;
-    if(cur_url.indexOf('/testdata')) {
-        if (sort_by) {
-            window.location.href = '/testdata?project_name=' + prj_name + "&version=" + build_version + "&sort_by=" + sort_by;
-        }
-    }
-});
-
-$("select[name='td_host']").change( function() {
-    var host_name = $(this).find("option:selected").attr('data-host_name');
-    if (host_name) {
-        $("#id_crash_host_name").val(host_name);
-    }
-});
-
-$("select[name='td_testcase']").change( function() {
-    var $selected_tc_option = $(this).find("option:selected")
-    var testcase_name = $selected_tc_option.attr('data-testcase_name');
-    var testcase_platform = $selected_tc_option.attr('data-testcase_platform');
-    if (testcase_name && testcase_platform) {
-        $("#id_crash_testcase_name").val(testcase_name);
-        $("#id_crash_testcase_platform").val(testcase_platform);
-    }
-});
-
-$('button.add_btn').click(function(){
-    if ($(this).html() == 'Add') {
-        var $add_sel = $(this).parent().find("select");
-        var $inputs = $(this).closest('tr').nextUntil('tr.crash_head').find('input, select');
-        var is_input_empty = true;
-        $inputs.each(function(){
-            if ($(this).attr('type') != 'hidden' && $(this).val()) {
-                is_input_empty = false;
-                return false;
-            }
-            return true;
-        });
-        if (is_input_empty) {
-            $(this).closest('tr').nextUntil('tr.crash_head').toggleClass('hidden');
-            $(this).html('Extend');
-            $(this).removeClass('btn btn-primary');
-            $(this).addClass('btn btn-default');
-            return;
-        }
-        var data_content = $inputs.serialize();
-        var url = $(this).attr('action');
-        var $add_btn = $(this);
-        if (url) {
-            ajax_post_data(url, data_content, function(items){
-                var first_option = $add_sel.find('option').first().html();
-                update_select_options($add_sel, items);
-                $inputs.each(function(){
-                    if ($(this).attr('type') != 'hidden' && $(this).val()) {
-                        $(this).val('');
-                        $(this).selectpicker('refresh');
-                    }
-                    return true;
-                });
-                $add_sel.trigger('change');
-                $add_btn.closest('tr').nextUntil('tr.crash_head').toggleClass('hidden');
-                $add_btn.html('Extend');
-                $add_btn.removeClass('btn btn-primary');
-                $add_btn.addClass('btn btn-default');
-                var $pop_element = $add_btn.siblings('div.bootstrap-select');
-                $pop_element.popover({'placement': 'top', 'content': 'add successfully!'});
-                $pop_element.popover('show');
-                $pop_element.one('click', function(){
-                    $pop_element.popover('destroy');
-                });
-            });
-        } else {
-            alert_box("Unknown Action", "Not found URL in action attribute!");
-        }
-    } else {
-        $(this).html('Add');
-        $(this).removeClass('btn btn-default');
-        $(this).addClass('btn btn-primary');
-        $(this).closest('tr').nextUntil('tr.crash_head').toggleClass('hidden');
-    }
-});
-
-$('button.del_btn').click(function(){
-    var url = $(this).attr('action');
-    var $select = $(this).parent().find("select");
-    var $del_option = $select.find("option:selected");
-    
-    var data_attrs = $del_option.getDataAttrs();
-    var data_count = get_size(data_attrs);
-    var $inputs = $(this).closest('tr').nextUntil('tr.crash_head').find('input[type="hidden"]');
-    $inputs.each(function(){
-        data_attrs[$(this).attr('name')] = $(this).val();
-    });
-    if (url) {
-        if (data_count) {
-            confirm_box("Delete?", "Do you confirm you really want to delete it?", function() {
-                ajax_post_data(url, data_attrs, function(items){
-                    //alert_box('Delete', "done!");
-                    update_select_options($select, items);
-                });
-            });
-        } else {
-            alert_box("Not select", "Not select any target!");
-        }
-    } else {
-        alert_box("Unknown Action", "Not found URL in action attribute!");
-    }
-});
-
-$('a>span.glyphicon-remove').click(function(){
-    var $a_link = $(this).parent();
-    confirm_box("Delete?", "Do you confirm you really want to delete it?", function() {
-        var data_attrs = $a_link.getDataAttrs();
-        data_attrs['csrfmiddlewaretoken'] = get_cookie('csrftoken');
-        var goto_url = data_attrs.url;
-        delete data_attrs.url;
-        var $form = $('<form action="' + goto_url + '" method="post"></form>');
-        for (attr in data_attrs) {
-            $form.append('<input type="hidden" name="' + attr + '" value="' + data_attrs[attr] + '"/>');
-        }
-        $form.submit();
-        //window.location.href = goto_url;
-    });
-    return false;
-});
-
-$('tr.build_head').click(function(){
-    // alert($(this).nextUntil('tr.build_head').length)
-    $(this).nextUntil('tr.build_head').toggleClass('hidden');
-});
-
-
 
 $(function(){
     if (typeof hosts != 'undefined') {
@@ -354,29 +201,5 @@ $(function(){
     }
     if (typeof builds != 'undefined') {
         update_select_options($('select[name="td_build_version"]'), builds);
-    }
-    if ($('#id_project_running_table').length > 0) {
-        $('#id_project_running_table').Tabledit({
-            url: '/ajax/edit_running_prj',
-            columns: {
-                identifier: [1, 'name'],
-                editable: [[2, 'running_build', '{}'], [3, 'total_devices']]
-            },
-            restoreButton: false,
-            onSuccess: function (data, textStatus, jqXHR) {
-                console.log(data);
-                console.log(textStatus);
-                console.log(jqXHR);
-            },
-            onDraw: function () {
-                console.log("onDraw!");
-                $('#id_project_running_table tr').each(function() {
-                    var $tableedit_select = $(this).find("td.tabledit-view-mode select");
-                    $tableedit_select.append($(this).find("td.hidden>select>option"));
-                    
-                });
-            }
-        });
-        $('#id_project_running_table').append("<input class='tabledit-input' type='hidden' name='" + 'csrfmiddlewaretoken' + "' value='" + get_cookie('csrftoken') + "'/>")
     }
 });
--- tbd/views.py
+++ tbd/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
 from tbd.models import Project, TestAction, Build, Crash, TestCase, Host, JIRA, TestResult
 from .forms import AddProjectForm, AddBuildForm, AddCrashForm, AddHostForm, AddTestCaseForm
 import time
@@ -1413,15 +1413,13 @@ def auto_crash_info(request):
     return json_response(msg, err_code)
 
 # Create your views here.
-@ensure_csrf_cookie
 def home_page(request):
     #cur_page = request.GET.get('page', 1)
     #item_per_page = 20
     #page_data = slice_page(Project.objects.all().order_by('-create'), cur_page, item_per_page)
     #return render(request, 'tbd/home.html', {'page': page_data})
     return render(request, 'tbd/home.html')
-
-@ensure_csrf_cookie
+        
 def project_page(request):
     #if request.method == 'POST':
     #    return project_page_post(request)
@@ -1459,7 +1457,6 @@ def project_page_post(request):
             flash(request, {'type': 'danger', 'msg': flash_err})
     return redirect('tbd_project')
 
-@ensure_csrf_cookie
 def build_page(request):
     #if request.method == 'POST':
     #    return build_page_post(request)
@@ -1475,7 +1472,6 @@ def build_page(request):
             target_prj = target_prj[0]
     return render(request, 'tbd/build.html', {'project': target_prj, 'projects': projects})
 
-@ensure_csrf_cookie
 def host_page(request):
     prj_id = request.GET.get('prj_id', '')
     build_id = request.GET.get('build_id', '')
@@ -1496,7 +1492,6 @@ def host_page(request):
             builds = Build.objects.filter(project=target_prj, is_stop=False).order_by('-create')
     return render(request, 'tbd/host.html', {'project': target_prj, 'projects': projects, 'build': target_build, 'builds': builds})
 
-@ensure_csrf_cookie
 def testaction_page(request):
     prj_id = request.GET.get('prj_id', '')
     build_id = request.GET.get('build_id', '')
@@ -1521,7 +1516,6 @@ def testaction_page(request):
         'testcase_platform_choice': json.dumps(dict(TestCase.PLATFORM_CHOICE)),
     })
 
-@ensure_csrf_cookie
 def testcase_page(request):
     prj_id = request.GET.get('prj_id', '')
     build_id = request.GET.get('build_id', '')
@@ -1550,7 +1544,6 @@ def testcase_page(request):
         'all_testactions': json.dumps([{'Value': ta.id, 'DisplayText': ta.name} for ta in all_testactions]),
     })
 
-@ensure_csrf_cookie
 def testresult_page(request):
     prj_id = request.GET.get('prj_id', '')
     build_id = request.GET.get('build_id', '')
@@ -1565,7 +1558,6 @@ def testresult_page(request):
         'project': target_prj, 'projects': projects,
     })
 
-@ensure_csrf_cookie
 def crash_page(request):
     prj_id = request.GET.get('prj_id', '')
     build_id = request.GET.get('build_id', '')
